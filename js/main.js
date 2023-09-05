6021a71164507e99f03170084ce0af5bd09ffbab
soyalexrodriguez <mr.rodriguez10@outlook.com>
2023-09-04 22:37:29 -0500

    termine la primera version del portafolio web

--- js/main.js
+++ js/main.js
@@ -8,11 +8,15 @@ function crearGotaDeLLuvia() {
     const gota = document.createElement('span')
     gota.className = 'gota'
     gota.textContent = Math.random() > 0.5 ? '0':'1'
+    gota.style.left = `${Math.random() * 100}vw`
+    gota.style.animationDuration = `${Math.random() * 5 + 3}s`
+    gota.style.zIndex = randomZindex()
     return gota
 }
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 45; i++) {
     const gota = crearGotaDeLLuvia();
+    gota.style.animationDelay = `${Math.random() * 5}s`
     lluvia.appendChild(gota)
     
 }
