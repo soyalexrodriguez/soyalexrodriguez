4691b950355be4df4e1910207571f58126929e0a
soyalexrodriguez <mr.rodriguez10@outlook.com>
2023-10-03 16:58:43 -0500

    Agregue un boton para extraer diferentes palabras aleatorias en el proyecto jergas peruanas

--- jergasperuanas/js/script.js
+++ jergasperuanas/js/script.js
@@ -1,37 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-     fetch("https://apiperu.fly.dev/")
-         .then(response => response.json())
-         .then(dataFromJson => {
-             // Generar un número aleatorio entre 0 y 210 (la longitud del array)
-             const randomIndex = Math.floor(Math.random() * dataFromJson.length);
+const API = "https://apiperu.fly.dev/"
 
-            const cardTitle = document.getElementById("cardTitle");
-            const etiquetaParrafo = document.getElementById("parrafo");
+async function fetchSlangs(urlApi) {
+    const resonse = await fetch(urlApi)
+    const data = resonse.json()
+    return data
+}
 
-             // Obtener datos del JSON en base al índice aleatorio
-             const palabra = dataFromJson[randomIndex].palabra;
-             const significado = dataFromJson[randomIndex].significado;
-          
-             // Actualizar contenido de la tarjeta con los datos aleatorios
-             cardTitle.innerText = palabra
-             const parrafo = significado.map(sig => `${sig}`).join("");
-             const numeroDeLetras = parrafo.length
-             
+(async () => {
+    try {
+        const dataFromJson = await fetchSlangs(API)
+         
+        // Generar un número aleatorio entre 0 y 210 (la longitud del array)
+        const randomIndex = Math.floor(Math.random() * dataFromJson.length);
 
-             if (numeroDeLetras >= 50) {
-                let parrafoTruncado = parrafo.substring(0,50) + "<b id='negrita'>...</b>"
-                let finalParrafo = "<span id='spanfinal' class='noshow'>" + parrafo.substring(50,numeroDeLetras) + "</span>"
-                etiquetaParrafo.innerHTML = parrafoTruncado + finalParrafo
-                console.log(parrafoTruncado + finalParrafo)
-             } else {
-                etiquetaParrafo.innerHTML = parrafo
-             }
-             console.log(numeroDeLetras)
-         })
-         .catch(error => {
-             console.error("Error al obtener los datos desde el API:", error);
-         });
-});
+        const cardTitle = document.getElementById("cardTitle");
+        const etiquetaParrafo = document.getElementById("parrafo");
+        const update = document.getElementById("update");
+
+        // Obtener datos del JSON en base al índice aleatorio
+        const palabra = dataFromJson[randomIndex].palabra;
+        const significado = dataFromJson[randomIndex].significado;
+      
+        // Actualizar contenido de la tarjeta con los datos aleatorios
+        cardTitle.innerText = palabra
+        const parrafo = significado.map(sig => `${sig}`).join("");
+        const numeroDeLetras = parrafo.length
+
+        if (numeroDeLetras >= 50) {
+            let parrafoTruncado = parrafo.substring(0,50) + "<b id='negrita'>...</b>"
+            let finalParrafo = "<span id='spanfinal' class='noshow'>" + parrafo.substring(50,numeroDeLetras) + "</span>"
+            etiquetaParrafo.innerHTML = parrafoTruncado + finalParrafo
+        } else {
+            etiquetaParrafo.innerHTML = parrafo
+        }
+        update.value = "Siguiente palabra ➡"
+    } catch (error) {
+        console.error("Error al obtener los datos desde el API:", error);
+        const update = document.getElementById("update");
+        update.value = "Actualizar 🔄"
+    }
+})()
+
+
 
 function toggleContent(box) {
     const puntos = document.getElementById('negrita')
@@ -39,4 +49,8 @@ function toggleContent(box) {
     box.children.cardBody.children.parrafo.classList.toggle('expanded')
     spanfinal.classList.toggle('clicked')
     puntos.classList.toggle('negritac')
+}
+
+function getAnotherWord() {
+    location.reload()
 }
